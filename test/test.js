a0bb82aba4e8a76d25fb1ed5a66c12854836b87a
Haley Townsend <haleyrk@hotmail.com>
2017-10-19 21:25:02 -0500

    different test

--- test/test.js
+++ test/test.js
@@ -14,14 +14,24 @@ const storage = server.storage;
 //allow for http requests
 chai.use(chaiHttp);
 
+//
+// describe('Hit root url', function() {
+//   it('should return 200', function() {
+//     return chai.request(app)
+//   })
+// })
 
-describe('Hit root url', function() {
-  it('should return 200', function() {
-    return chai.request(app)
-  })
-})
-
-
+describe('index page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
+});
 
 //describe('storm', function() {
 
