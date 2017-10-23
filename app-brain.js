4c0db3576432df6be80de0e0b66dbecc3f3f01cb
Haley Townsend <haleyrk@hotmail.com>
2017-10-23 13:38:38 -0500

    mock data

---
+++ app-brain.js
@@ -0,0 +1,59 @@
+var USER_ENTRIES = {
+  "userEntries": [
+    {
+      "id": "1111",
+      "migraine": true,
+      "painIntensity": 4,
+      "medication": [
+        {
+          "sumitriptan": false,
+          "excedrin": false,
+          "other": false,
+      },
+      "journalText": "The users journal with daily notes will be here.",
+      "publishedAt": 147001697669
+    },
+    {
+      "id": "2222",
+      "migraine": false,
+      "painIntensity": 0,
+      "medication": [
+        {
+          "sumitriptan": false,
+          "excedrin": false,
+          "other": false,
+      },
+      "journalText": "User 2's journal with daily notes will be here.",
+      "publishedAt": 147001697669
+    },
+    {
+      "id": "3333",
+      "migraine": true,
+      "painIntensity": 7,
+      "medication": [
+        {
+          "sumitriptan": false,
+          "excedrin": false,
+          "other": false,
+      },
+      "journalText": "User 3's journal with daily notes will be here.",
+      "publishedAt": 147001697669
+    },
+    {
+      "id": "4444",
+      "migraine": false,
+      "painIntensity": 0,
+      "medication": [
+        {
+          "sumitriptan": false,
+          "excedrin": false,
+          "other": false,
+      },
+      "journalText": "User 4's journal with daily notes will be here.",
+      "publishedAt": 147001697669
+    },
+  ]
+};
+
+
+function
