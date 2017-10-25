1d398d2c26d53558f36c6abd6d22316b35ed0151
Haley Townsend <haleyrk@hotmail.com>
2017-10-25 17:35:51 -0500

    layout now bootstrap

--- public/index.js
+++ public/index.js
@@ -1,25 +1,25 @@
-var API_BASE_URL ="http://api.openweathermap.org/data/2.5/weather?";
-
-function loadWeatherData () {
-  return $.ajax(API_BASE_URL, {
-    data: {
-      "q": 'KansasCity',
-      "appId": '431f20e3dec4bfcfd571665b88c0f488',
-      "weather": {
-        "main": "Clouds",
-        "icon": "04d"
-        },
-      "main": {
-        "temp": 40,
-        "pressure": 100
-        },
-      }
-    })
-    .then(function (data) {
-      console.log(loadWeatherData())
-    });
-  }
-
-$(document).ready(function() {
-  loadWeatherData();
-})
+// var API_BASE_URL ="http://api.openweathermap.org/data/2.5/weather?";
+//
+// function loadWeatherData () {
+//   return $.ajax(API_BASE_URL, {
+//     data: {
+//       "q": 'KansasCity',
+//       "appId": '431f20e3dec4bfcfd571665b88c0f488',
+//       "weather": {
+//         "main": "Clouds",
+//         "icon": "04d"
+//         },
+//       "main": {
+//         "temp": 40,
+//         "pressure": 100
+//         },
+//       }
+//     })
+//     .then(function (data) {
+//       console.log(loadWeatherData())
+//     });
+//   }
+//
+// $(document).ready(function() {
+//   loadWeatherData();
+// })
